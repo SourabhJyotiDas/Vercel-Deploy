81ba55490847af464634d3f1dd57777140a64cd3
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-25 11:14:42 +0530

    asdsadsad

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectToDatabase = async () => {
       try {
-            await mongoose.connect(process.env.MONGOURI);
+            await mongoose.connect("mongodb://localhost:27017/verceldeploy");
             console.log("Mongoose Connected Sucessfully");
       } catch (error) {
             console.log(error);
