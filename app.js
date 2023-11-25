58ee51d73d638b0cd0b92c8966ed50ad6f1ceb28
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-25 11:07:37 +0530

    sdfsdfds

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(cors());
 app.use(
    cors({
-      origin: "*",
+      origin: ["*"],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
    })
