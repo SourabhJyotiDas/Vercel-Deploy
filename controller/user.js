0d1f4332844c5b70c07e3d06e302e79f90afc1d4
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-25 10:55:38 +0530

    asaS

--- controller/user.js
+++ controller/user.js
@@ -31,6 +31,8 @@ export const registerUser = async (req, res) => {
       const options = {
          expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
          httpOnly: true,
+         secure: true,
+         sameSite: "none",
       };
 
       res.status(200).cookie("token", token, options).json({ success: true, user })
